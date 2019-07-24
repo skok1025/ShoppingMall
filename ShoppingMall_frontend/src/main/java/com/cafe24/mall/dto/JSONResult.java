11bf1ef24194c10dcc4c114bd00f9c422238471e
kim <skok1025@naver.com>
2019-07-24 17:17:27 +0900

    frontEnd JSONResult<T> 추가

---
+++ ShoppingMall_frontend/src/main/java/com/cafe24/mall/dto/JSONResult.java
@@ -0,0 +1,30 @@
+package com.cafe24.mall.dto;
+
+public class JSONResult<T> {
+	private String result;
+	private String message;
+	private T data;
+	
+	public String getResult() {
+		return result;
+	}
+	public void setResult(String result) {
+		this.result = result;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public T getData() {
+		return data;
+	}
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	
+
+	
+}
