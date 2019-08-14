9ae66c468c6f08aae277f194833023c51d4424a8
kim <skok1025@naver.com>
2019-08-14 21:19:45 +0900

    ~ 08.14 END

--- ShoppingMall_backend/src/main/java/com/cafe24/mall/config/WebConfig.java
+++ ShoppingMall_backend/src/main/java/com/cafe24/mall/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.cafe24.mall.config;
 
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.cafe24.config.web.MVCConfig;
 import com.cafe24.config.web.MessageConfig;
+import com.cafe24.config.web.MyZipkinProperties;
 import com.cafe24.config.web.SecurityConfig;
 import com.cafe24.config.web.SwaggerConfig;
 import com.cafe24.config.web.ZipkinConfig;
@@ -16,7 +18,8 @@ import com.cafe24.config.web.ZipkinConfig;
 @EnableAspectJAutoProxy
 @EnableWebMvc
 @ComponentScan({"com.cafe24.mall.controller","com.cafe24.mall.exception"})
-@Import({ MVCConfig.class, SecurityConfig.class, MessageConfig.class, SwaggerConfig.class/* ,ZipkinConfig.class */})
+@EnableConfigurationProperties(MyZipkinProperties.class)
+@Import({ MVCConfig.class, SecurityConfig.class, MessageConfig.class, SwaggerConfig.class /* ,ZipkinConfig.class */  })
 public class WebConfig{
 	
 }
