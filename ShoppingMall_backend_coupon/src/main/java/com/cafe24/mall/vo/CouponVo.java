c77c447f410fb2f3be3220aee88cd1ca8e533016
admin <admin@DESKTOP-6FJ1ABV>
2021-06-21 21:36:53 +0900

    커넥션 정보 변경 & get, set, toString 롬복에서 원복

--- ShoppingMall_backend_coupon/src/main/java/com/cafe24/mall/vo/CouponVo.java
+++ ShoppingMall_backend_coupon/src/main/java/com/cafe24/mall/vo/CouponVo.java
@@ -11,7 +11,6 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Data
 public class CouponVo {
 	private Long no;
 	private String info_no;
@@ -24,7 +23,95 @@ public class CouponVo {
 	private String upt_timestamp;
 	private String is_delete;
 	private List<String> memberNoList;
-	
+
+	public Long getNo() {
+		return no;
+	}
+
+	public void setNo(Long no) {
+		this.no = no;
+	}
+
+	public String getInfo_no() {
+		return info_no;
+	}
+
+	public void setInfo_no(String info_no) {
+		this.info_no = info_no;
+	}
+
+	public String getMember_no() {
+		return member_no;
+	}
+
+	public void setMember_no(String member_no) {
+		this.member_no = member_no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSale_type() {
+		return sale_type;
+	}
+
+	public void setSale_type(String sale_type) {
+		this.sale_type = sale_type;
+	}
+
+	public String getSale_value() {
+		return sale_value;
+	}
+
+	public void setSale_value(String sale_value) {
+		this.sale_value = sale_value;
+	}
+
+	public String getIs_used() {
+		return is_used;
+	}
+
+	public void setIs_used(String is_used) {
+		this.is_used = is_used;
+	}
+
+	public String getIns_timestamp() {
+		return ins_timestamp;
+	}
+
+	public void setIns_timestamp(String ins_timestamp) {
+		this.ins_timestamp = ins_timestamp;
+	}
+
+	public String getUpt_timestamp() {
+		return upt_timestamp;
+	}
+
+	public void setUpt_timestamp(String upt_timestamp) {
+		this.upt_timestamp = upt_timestamp;
+	}
+
+	public String getIs_delete() {
+		return is_delete;
+	}
+
+	public void setIs_delete(String is_delete) {
+		this.is_delete = is_delete;
+	}
+
+	public List<String> getMemberNoList() {
+		return memberNoList;
+	}
+
+	public void setMemberNoList(List<String> memberNoList) {
+		this.memberNoList = memberNoList;
+	}
+
 	@Override
 	public String toString() {
 		return "CouponVo [no=" + no + ", info_no=" + info_no + ", member_no=" + member_no + ", name=" + name
